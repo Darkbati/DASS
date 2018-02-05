7191483e68c139bc56898830e145369003a3c16d
Darkbati <fifainte@nate.com>
2018-02-06 01:25:12 +0900

    commit the Main Entry Source

--- DASS/src/main/java/com/liberty/dataserver/server/MainService.java
+++ DASS/src/main/java/com/liberty/dataserver/server/MainService.java
@@ -2,25 +2,23 @@ package com.liberty.dataserver.server;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.util.Assert;
 
-import com.liberty.dataserver.system.ShutdownHookThread;
+import com.liberty.dataserver.config.SpringContext;
 
 public class MainService {
 	static Log log = LogFactory.getLog(MainService.class.getName());
 
-	public static void main(String[] args) throws Exception {		
-		Runtime.getRuntime().addShutdownHook(new ShutdownHookThread());
-		/*
+	public static void main(String[] args) throws Exception {
+		// Runtime.getRuntime().addShutdownHook(new ShutdownHookThread());
 		GenericApplicationContext ctx = SpringContext.getContext();
 		Assert.notNull(ctx);
 
 		NettyServer netty = ctx.getBean(NettyServer.class);
 		netty.start();
-		*/
-		
-		throw new Exception("a");
 	}
-	
+
 	public static boolean isNumber(String s) {
 		try {
 			Long.parseLong(s);
